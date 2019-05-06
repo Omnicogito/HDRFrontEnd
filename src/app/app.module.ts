96ce956969ec05be202c9ed0fad6cc9cc660e1f4
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-05-06 13:33:23 -0400

    Doggo index done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,6 +9,7 @@ import { PersonComponent } from './components/person/person.component';
 import { KennelComponent } from './components/kennel/kennel.component';
 import { DoggoComponent } from './components/doggo/doggo.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DoggoService } from './services/doggo.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +23,12 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    DoggoService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/doggo.ts
@@ -0,0 +1,12 @@
+export interface Doggo {
+    DoggoID?: number;
+    Name: string;
+    Breed: string;
+    Size: string;
+    HumanID: string;
+    DoggoFriendly: boolean;
+    PeopleFriendly: boolean;
+    SpecialNeeds: string;
+    Age: string;
+    Image?: string;
+}
