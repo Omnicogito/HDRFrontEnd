e7f3feb2f93400927bc799ce65425e217ebd55fa
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-05-08 16:31:00 -0400

    Person and doggo

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -2,28 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Person } from '../models/person';
 
-const ApiUrl = 'humananddoggo.azurewebsites.net/api';
+const ApiUrl = 'https://humananddoggo.azurewebsites.net/api';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class PersonService {
-  constructor(private _http: HttpClient) { }
-
-  getPerson(id: string) {
-    return this._http.get(`${ApiUrl}/Person/${id}`, { headers: this.getHeaders() });
-  }
+  constructor(private http: HttpClient) { }
 
   getPersons() {
-    return this._http.get(`${ApiUrl}/Person` , { headers: this.getHeaders() });
+    return this.http.get(`${ApiUrl}/Person` , { headers: this.getHeaders() });
+  }
+
+  getPerson(id: string) {
+    return this.http.get(`${ApiUrl}/Person/${id}`, { headers: this.getHeaders() });
   }
 
   createPerson(person: Person) {
-    return this._http.post(`${ApiUrl}/Person`, person, { headers: this.getHeaders() });
+    return this.http.post(`${ApiUrl}/Person`, person, { headers: this.getHeaders() });
   }
   updatePerson(person: Person) {
-    return this._http.put(`${ApiUrl}/Person`, person, { headers: this.getHeaders() });
+    return this.http.put(`${ApiUrl}/Person`, person, { headers: this.getHeaders() });
   }
   deletePerson(id: number) {
-    return this._http.delete(`${ApiUrl}/Person/${id}`, { headers: this.getHeaders() });
+    return this.http.delete(`${ApiUrl}/Person/${id}`, { headers: this.getHeaders() });
   }
   private getHeaders() {
     return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
