2e2293b55161cb54b50dc41b9110724b5b906c03
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-05-12 23:29:48 -0400

    Home worked on

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  messageForm: FormGroup;
+  submitted = false;
+  success = false;
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      message: ['', Validators.required],
+      email: ['', Validators.required],
+      interest: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.messageForm.invalid) {
+      return;
+    }
+
+    this.http
+       .post('https://formspree.io/your@email.com', {
+         name: this.messageForm.value.name,
+         interest: `${this.messageForm.value.interest}`,
+         email: this.messageForm.value.email,
+         message: this.messageForm.value.message
+       });
+  }
   ngOnInit() {
   }
 
