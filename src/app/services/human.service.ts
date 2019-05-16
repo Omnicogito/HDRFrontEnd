0fa91d7cbffdcd2676dce0dc94009dab22e1349e
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-05-16 11:46:18 -0400

    publishing on Heroku

--- src/app/services/human.service.ts
+++ src/app/services/human.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Human } from '../models/human';
+import { APIURL } from '../../../src/environments/environment.prod';
 
 const ApiUrl = 'https://humananddoggo.azurewebsites.net/api';
 
@@ -11,23 +12,23 @@ export class HumanService {
   constructor(private http: HttpClient) { }
 
   getHumans() {
-    return this.http.get(`${ApiUrl}/Human` , { headers: this.getHeaders() });
+    return this.http.get(`${APIURL}/Human` , { headers: this.getHeaders() });
   }
 
   getHuman(id: string) {
 
-    return this.http.get(`${ApiUrl}/Human/${id}`, { headers: this.getHeaders() });
+    return this.http.get(`${APIURL}/Human/${id}`, { headers: this.getHeaders() });
   }
 
   createHuman(human: Human) {
-    return this.http.post(`${ApiUrl}/Human`, human, { headers: this.getHeaders() });
+    return this.http.post(`${APIURL}/Human`, human, { headers: this.getHeaders() });
   }
 
   updateHuman(human: Human) {
-    return this.http.put(`${ApiUrl}/Human`, human, { headers: this.getHeaders() });
+    return this.http.put(`${APIURL}/Human`, human, { headers: this.getHeaders() });
   }
   deleteHuman(id: number) {
-    return this.http.delete(`${ApiUrl}/Human/${id}`, { headers: this.getHeaders() });
+    return this.http.delete(`${APIURL}/Human/${id}`, { headers: this.getHeaders() });
   }
 
   private getHeaders() {
